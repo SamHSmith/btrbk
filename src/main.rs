85e8fa5e30c35d63e9fc433fca3f096b83784a42
Sam Smith <sam.henning.smith@protonmail.com>
2020-05-21 17:14:52 +0200

    Added btrfs support and rc 1
    Added btrfs support
    Added verbose cli argument
    I'm making this the 1.0 release candidate.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use std::env;
 use std::process::Command;
 use std::fs;
+use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-
 
     let _full_cont =fs::read_to_string("./.btrbk")
-        .expect("Something went wrong reading the file");
+        .expect("Can't read from ./btrbk");
     let contents : Vec<&str> = _full_cont.split("\n").collect();
 
     let mut from : String = "".to_string();
     let mut to   : String = "".to_string();
+    let mut btrfs : bool = false;
 
     for c in contents {
         let pair : Vec<&str>= c.split(":").collect();
@@ -22,12 +22,14 @@ fn main() {
         if pair.len() > 1 && pair[0].contains("backup_to") {
             to = pair[1].replace(" ", "");
         }
+        if pair.len() > 1 && pair[0].contains("btrfs") {
+            let btrstr = pair[1].replace(" ", "");
+            if btrstr.contains("yes"){
+                btrfs = true;
+            }
+        }
     }
 
-    cp(from, to);
-}
-
-fn cp(from : String, to: String){
     if from.len() == 0 {
         println!("backup_from must be set in .btrbk");
         return;
@@ -37,18 +39,66 @@ fn cp(from : String, to: String){
         return;
     }
 
-    let cmd = format!("cp -rp {} {}/{}", from, to, chrono::Utc::now().to_rfc2822().replace(" ", "_").replace(",", ""));
+    let pretime = chrono::Utc::now().to_rfc2822().replace(" ", "_").replace(",", "").split("+").collect::<Vec<&str>>()[0].to_string();
+    let mut index = 0;
 
-    exec_cmd(&cmd);
+    let from_path = Path::new(&from);
+    let mut to_path;
+    let mut _to_path_string;
+
+    loop {
+        _to_path_string = format!("{}/{}_{}", to, pretime, index);
+        if btrfs {
+            use std::ops::Add;
+            _to_path_string = _to_path_string.add("_btrfs");
+        }
+        to_path = Path::new(&_to_path_string);
+        if !to_path.exists() {
+            break;
+        }
+        index+=1;
+    }
+
+    let _verbose_string = "verbose".to_string();
+
+    if btrfs {
+        btrfs_cp(&from_path, to_path)
+    }else {
+        cp(&from_path, &to_path, args.contains(&_verbose_string));
+    }
 }
 
-fn exec_cmd(cmd : &str){
+fn btrfs_cp(from : &Path, to : &Path){
+    let cmd = format!("btrfs subv snapshot {} {}", from.to_str().unwrap(), to.to_str().unwrap());
+    let error = exec_cmd(&cmd);
+    if error.is_some() {
+        eprint!("{}", error.unwrap());
+        eprintln!("For btrfs backups to work, both backup_from and backup_to have to reside on a btrfs filesystem and backup_from needs to be a btrfs subvolume.");
+        eprintln!("You create a btrfs subvolume by using the command: btrfs subvolume create {{subvolume name}}");
+    }
+}
+
+fn cp(from : &Path, to: &Path, verbose: bool){
+    let cmd = format!("cp -rp{} {} {}", if verbose {"v"} else {""} , from.to_str().unwrap(), to.to_str().unwrap());
+    let err = exec_cmd(&cmd);
+    if err.is_some() {
+        eprint!("ERROR: {}", err.unwrap());
+    }
+}
+
+fn exec_cmd(cmd : &str) -> Option<String> {
 
     let output =Command::new("sh")
+        .stdout(std::process::Stdio::inherit())
         .arg("-c")
         .arg(cmd)
         .output()
         .expect("failed to execute process");
 
-    print!("{}", String::from_utf8(output.stdout).expect("Can't convert stdout to utf8 string."));
+    let error = String::from_utf8(output.stderr).expect("Can't convert stderr to utf8 string.");
+    if error.len() > 0 {
+        return Some(error);
+    } else {
+        return None;
+    }
 }
